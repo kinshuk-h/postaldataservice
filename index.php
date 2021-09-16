8bd9b8a531a1eeb7fdffb2c2d015f52c8aba668b
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2021-09-16 15:32:42 +0530

    Deployed to production

--- index.php
+++ index.php
@@ -174,10 +174,7 @@
             </section>
         </article>
         <article class="container block">
-            <?php
-                $old_pincodes = $postal_data->get_pincodes();
-                echo "<pre>".var_dump($old_pincodes)."</pre>";
-            ?>
+            <?php $old_pincodes = $postal_data->get_pincodes(); ?>
             <section class="box <?= count($old_pincodes) > 0 ? "" : "hidden" ?>">
                 <h4 class="subtitle">
                     List of Postal Codes previously recorded:
